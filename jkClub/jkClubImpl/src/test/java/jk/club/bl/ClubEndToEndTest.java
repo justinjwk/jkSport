2574c0dfbc1a252c993a988d7420b11c91369896
Justin Kim <justinjaekim@gmail.com>
2018-09-14 02:14:48 -0400

    completed proj#0

---
+++ jkClub/jkClubImpl/src/test/java/jk/club/bl/ClubEndToEndTest.java
@@ -0,0 +1,30 @@
+package jk.club.bl;
+
+import org.junit.Test;
+
+public class ClubEndToEndTest {
+
+	@Test
+	public void test() {
+		
+//		reset (using OrganizationTestUtil) -- reset the organization database to an initial starting state.
+//
+//		createParent (MemberMgmtImpl) - parent depicted as Individual
+//
+//		createPlayer (MemberMgmtImpl)
+//
+//		createCoach (MemberMgmtImpl)
+//
+//		createTeam (using OrgMgmtImpl)
+//
+//		assignPlayers (using OrgMgmtImpl)
+//
+//		assignCoach (using OrgMgmtImpl)
+//
+//		assignManager (using OrgMgmtImpl)
+//
+//		getTeamRoster (using OrgMgmtImpl)
+//
+//		getTeamSchedule (using OrgMgmtImpl) - this will later call eLeague for Contest information, but for now simply returns an empty list.
+	}
+}
---
+++ jkLeague/jkLeagueImpl/src/test/java/jk/league/bl/LeagueEndToEndTest.java
@@ -0,0 +1,38 @@
+package jk.league.bl;
+
+import org.junit.Test;
+
+public class LeagueEndToEndTest {
+
+	@Test
+	public void test() {
+		
+//		reset (using LeagueTestUtil) -- reset the eLeague database to an initial starting state.
+//
+//		ingest data (using LeagueIngestor) -- ingest the full XML test data file.
+//
+//		createClub (using LeagueMgmtImpl)
+//
+//		addVenue (using ClubMgmtImpl)
+//
+//		addTeam (using ClubMgmtImpl)
+//
+//		updateContact for team (using ClubMgmtImpl)
+//
+//		assignTeamDivision (using LeagueMgmtImpl) - divisions already exist for current/next season within the test data.
+//
+//		scheduleSeason (using ContestMgmtImpl)
+//
+//		getDivisions (using LeagueMgmtImpl)
+//
+//		getDivisionStandings (using LeagueMgmtImpl)
+//
+//		getTeam with schedule and results (using LeagueMgmtImpl)
+//
+//		reportScore (using ContestMgmtImpl)
+//
+//		getTeam with schedule and results (using LeagueMgmtImpl)
+//
+//		getDivisionStandings (using LeagueMgmtImpl)
+	}
+}
